b3161d7fa1d865db4e8ff655639380b147da0fc8
MikaHub <micael.dossantos@edu.itescia.fr>
2020-06-25 20:45:49 +0200

    Contact page

--- ec-code-2020-codflix-php-master/controller/contactController.php
+++ ec-code-2020-codflix-php-master/controller/contactController.php
@@ -4,25 +4,24 @@ require_once( 'model/user.php' );
 
 if (isset($_POST['send'])) {
     sendEmail($_POST['send']);
-    echo $_POST['send'];
 }
 
-function contact() {
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+function contactPage() {
+    $email = isset($_POST['email']);
+    $message = isset($_POST['message']);
 
     if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i", $email))
     {
-        $error_msg = "Format de mail non valide.";
+        $error_msg = "Mail non valide";
     }
     elseif (empty($message))
     {
-        $error_msg = "Veuillez saisir votre message.";
+        $error_msg = "Veuillez saisir votre message";
     }
     else
     {
         sendEmail($email, $message);
-        $success_msg = "Votre message a correctement été envoyé à notre équipe. Une réponse vous sera apportée dans les plus bref délais.";
+        $success_msg = "Votre message a bien été envoyé à notre équipe";
     } 
 
     require('view/auth/contactView.php');
